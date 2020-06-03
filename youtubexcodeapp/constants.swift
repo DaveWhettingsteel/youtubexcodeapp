9eb426a87758103c8526d68079450ce71bac4d2c
Dave Whettingsteel <dwhettingsteel@westnet.com.au>
2020-06-03 11:06:40 +0800

    Tom Court Playlist

--- youtubexcodeapp/constants.swift
+++ youtubexcodeapp/constants.swift
@@ -10,7 +10,16 @@ import Foundation
 
 struct Constants {
     
+    
+    
+    
     static var API_KEY = "AIzaSyAOsNVCTe31tnWlxZ7xc-26XKHbX3egULU"
-    static var PLAYLIST_ID = "PLMRqhzcHGw1aLoz4pM_Mg2TewmJcMg9ua"
+    
+    // Tom Court PLay List
+    
+    static var PLAYLIST_ID = "PLnkXOJiZ9KuP_64VYTDY1-J5P3iKXYCrH"
+          
+    static var CWC_PLAYLIST_ID = "PLMRqhzcHGw1aLoz4pM_Mg2TewmJcMg9ua"
+    
     static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
 }
